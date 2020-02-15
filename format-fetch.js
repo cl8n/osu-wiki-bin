f61f18a0e2a7bb00c3bb8cbd58d3d3292379df0b
clayton <me@clayton.cc>
2020-02-14 16:07:37 -0800

    Move replace utility to a format command

---
+++ format-fetch.js
@@ -0,0 +1,35 @@
+const { readFile, writeFile } = require('fs').promises;
+const { beatmapLink, beatmapsetLink, userLink } = require('./include');
+
+function run(options) {
+    return Promise.all(options._.map(file => replace(file, !options['no-flags'])));
+}
+
+async function replace(file, flag) {
+    let content = await readFile(file, 'utf8');
+
+    for (const match of content.matchAll(/b#(\d+)/g))
+        content = content.replace(match[0], await beatmapLink(match[1]));
+
+    for (const match of content.matchAll(/s#(\d+)/g))
+        content = content.replace(match[0], await beatmapsetLink(match[1]));
+
+    for (const match of content.matchAll(/u#(\d+)|u!(.+?)!u/g)) {
+        const byName = match[1] === undefined;
+        content = content.replace(
+            match[0],
+            await userLink(match[byName ? 2 : 1], { byName, flag })
+        );
+    }
+
+    await writeFile(file, content);
+}
+
+module.exports = {
+    name: 'fetch',
+    options: {
+        f: '=no-flags',
+        'no-flags': 'boolean'
+    },
+    run
+};
