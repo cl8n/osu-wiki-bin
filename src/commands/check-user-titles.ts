485997fa881e0d34aa77e0caaf0d77351d55b9a0
clayton <me@clayton.cc>
2020-10-14 11:11:07 -0700

    Move check-user-titles

---
+++ src/commands/check-user-titles.ts
@@ -0,0 +1,25 @@
+import { Command } from 'commander';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { wikiPath } from '../wiki';
+import { scrapeUser } from '../../include';
+
+// TODO: would be nice to also check titles missing from the article, but this isn't possible with web api yet
+async function checkUserTitles() {
+    const file = join(wikiPath, 'wiki/People/Users_with_unique_titles/en.md');
+    const content = (await readFile(file, 'utf8')).split('## By name')[1];
+
+    for (const match of content.matchAll(/users\/(?<id>\d+)\) \| (?<title>.+?) \|$/gm)) {
+        const matchGroups = match.groups as { id: string; title: string; };
+        const user = await scrapeUser(matchGroups.id);
+
+        if (user.title !== matchGroups.title)
+            console.log(user.username);
+    }
+}
+
+export function checkUserTitlesCommandBuilder() {
+    return new Command('check-user-titles')
+        .description('Report differences between Users_with_unique_titles and titles on web')
+        .action(checkUserTitles);
+}
--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,8 +1,10 @@
+import { checkUserTitlesCommandBuilder } from './check-user-titles';
 import { findBrokenRefsCommandBuilder } from './find-broken-refs';
 import { sortListsCommandBuilder } from './sort-lists';
 import { updateFlagsCommandBuilder } from './update-flags';
 
 export const commandBuilders = [
+    checkUserTitlesCommandBuilder,
     findBrokenRefsCommandBuilder,
     sortListsCommandBuilder,
     updateFlagsCommandBuilder,
