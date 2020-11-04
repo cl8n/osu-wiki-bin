bff78de3308fa8dfb78334445b10924fb4eeddad
clayton <me@clayton.cc>
2020-11-03 19:06:23 -0800

    Fix git function typing

--- src/git.ts
+++ src/git.ts
@@ -1,8 +1,8 @@
 import { execFile } from 'child_process';
 import { wikiPath } from './wiki';
 
-export function git(...args: string[]) {
-    return new Promise<string>((resolve, reject) => {
+export function git(args: string[]): Promise<string> {
+    return new Promise((resolve, reject) => {
         execFile('git', args, { cwd: wikiPath }, (error, stdout, stderr) => {
             if (error)
                 return reject(error);
