654cd23c0c3f37fe45bfe17cf40432bdb7d060d0
clayton <me@clayton.cc>
2020-12-13 00:08:02 -0800

    Better performance and error logging for "replace"

--- src/commands/replace.ts
+++ src/commands/replace.ts
@@ -2,6 +2,8 @@ import { Command } from 'commander';
 import { promises as fsPromises } from 'fs';
 const { readFile, writeFile } = fsPromises;
 import { beatmapLink, beatmapsetLink, userLink } from '../api';
+import { errorX } from '../console';
+import { getFiles } from '../files';
 
 interface ReplaceOptions {
     flags: boolean;
@@ -9,29 +11,48 @@ interface ReplaceOptions {
 
 async function replaceInFile(path: string, flag: boolean) {
     let content = await readFile(path, 'utf8');
+    const originalContent = content;
 
     for (const match of content.matchAll(/b#(\d+)/g))
-        content = content.replace(match[0], await beatmapLink(match[1]));
+        try {
+            content = content.replace(match[0], await beatmapLink(match[1]));
+        } catch (e) {
+            errorX(`Failed to replace beatmap #${match[1]} in ${path}: ${e.message}`);
+        }
 
     for (const match of content.matchAll(/s#(\d+)/g))
-        content = content.replace(match[0], await beatmapsetLink(match[1]));
+        try {
+            content = content.replace(match[0], await beatmapsetLink(match[1]));
+        } catch (e) {
+            errorX(`Failed to replace beatmapset #${match[1]} in ${path}: ${e.message}`);
+        }
 
     for (const match of content.matchAll(/u#(\d+)|u!(.+?)!u/g)) {
         const byName = match[1] === undefined;
-        content = content.replace(
-            match[0],
-            await userLink(match[byName ? 2 : 1], { byName, flag })
-        );
+        const id = match[byName ? 2 : 1];
+
+        try {
+            content = content.replace(
+                match[0],
+                await userLink(id, { byName, flag }),
+            );
+        } catch (e) {
+            errorX(`Failed to replace user ${byName ? '' : '#'}${id} in ${path}: ${e.message}`);
+        }
     }
 
-    await writeFile(path, content);
+    if (content !== originalContent)
+        await writeFile(path, content);
 }
 
 async function replace(paths: string[], options: ReplaceOptions) {
     if (paths.length === 0)
         paths.push('.');
 
-    await Promise.all(paths.map(file => replaceInFile(file, options.flags)));
+    await Promise.all(
+        (await getFiles(paths, 'md'))
+            .map(file => replaceInFile(file, options.flags))
+    );
 }
 
 export function replaceCommandBuilder() {
