6f70f424fe3345c0e60b236f7785e16b88d59199
clayton <me@clayton.cc>
2022-01-17 06:38:56 -0800

    Add module for working with osu-wiki remark installation

---
+++ src/remark.ts
@@ -0,0 +1,79 @@
+import { join } from 'path';
+import { error } from './console';
+import { wikiPath } from './wiki';
+
+const wikiImports: Record<string, any> = {};
+let processor: any;
+
+async function getProcessor() {
+  if (processor == null) {
+    const imports = await getImports();
+    const newPlugins = [];
+    const remarkRc = await import(join(wikiPath, '.remarkrc'));
+
+    for (const plugin of remarkRc.plugins) {
+      if (Array.isArray(plugin)) {
+        if (plugin[1] === false || plugin[0].match(/^(?:preset-)?lint-/) != null) {
+          continue;
+        }
+
+        newPlugins.push([
+          imports['remark-' + plugin[0]].default,
+          plugin[1],
+        ]);
+      } else {
+        if (plugin.match(/^(?:preset-)?lint-/) != null) {
+          continue;
+        }
+
+        newPlugins.push(imports['remark-' + plugin].default)
+      }
+    }
+
+    processor = imports.remark.default()
+      .use({
+        plugins: newPlugins,
+        settings: remarkRc.settings,
+      })
+      .freeze();
+  }
+
+  return processor;
+}
+
+async function importFromWiki(moduleName: string) {
+  const path = join(wikiPath, 'node_modules', moduleName);
+  const module = await import(path)
+    .catch(() => {
+      error(`${moduleName} is not installed in osu-wiki. Run \`npm install\`.`, 1);
+    });
+
+  wikiImports[moduleName] = module;
+}
+
+export async function getImports() {
+  if (!Object.isFrozen(wikiImports)) {
+    const moduleNames = [
+      'remark',
+      'remark-frontmatter',
+      'remark-gfm',
+      'to-vfile',
+      'unist-util-visit',
+    ];
+
+    await Promise.all(moduleNames.map((module) => importFromWiki(module)));
+    Object.freeze(wikiImports);
+  }
+
+  return wikiImports;
+}
+
+export async function getMdAst(filename: string) {
+  const processor = await getProcessor();
+  const vfile = (await getImports())['to-vfile'].readSync(filename);
+
+  return await processor.run(
+    processor.parse(vfile),
+    vfile,
+  );
+}
